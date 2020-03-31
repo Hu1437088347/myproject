40df58fd96c267178f258defefc2965affce3495
unknown <1437088347@qq.com>
2020-03-31 13:38:32 +0800

    作业3 09-huliyang finish time stamp

--- post_youdao.py
+++ post_youdao.py
@@ -2,15 +2,29 @@ import requests
 
 
 url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
+
+
+def get_salt():
+    return '15853783488690'
+
+
+def get_sing():
+    return '6b9a6ce3ff157252d9a388c507e70474'
+
+
+def get_ts():
+    return '1585378348869'
+
+
 form_data={
     'i':'我们都爱你',
     'from': 'AUTO',
     'to': 'AUTO',
     'smartresult': 'dict',
     'client': 'fanyideskweb',
-    'salt': '15853783488690',
-    'sign': '6b9a6ce3ff157252d9a388c507e70474',
-    'ts': '1585378348869',
+    'salt': get_salt(),
+    'sign': get_sing(),
+    'ts': get_ts(),
     'bv': '70244e0061db49a9ee62d341c5fed82a',
     'doctype': 'json',
     'version': '2.1',
