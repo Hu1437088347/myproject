7963bed29fd29226f4e2dbd79fc1d259287219c5
unknown <1437088347@qq.com>
2020-04-13 15:34:15 +0800

    作业5 09-huliyang add post youdao

--- post_youdao.py
+++ post_youdao.py
@@ -2,43 +2,87 @@ import random
 import requests
 import time
 
+#
+# url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
+# content="我和你"
 
-url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
+class Youdao():
+    def __init__(self,content):
+        self.get_content=content
+        self.url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
+        self.ts=self.get_ts()
+        self.salt=self.get_salt()
+        self.sign=self.get_sign()
+
+    def get_salt(self):
+        s=str(random.randint(0,10))
+        t=self.ts
+        # print("random = ",s)
+        # print("ts= ",t)
+        # print("salt= ",t+s)
+        return  t+s
+        # return '15846848803956'
+    def get_md5(self,value):
+        import hashlib
+        m = hashlib.md5()
+        m.update(value.encode("utf-8"))
+        return m.hexdigest()
 
 
-def get_salt():
-    s=str(random.randint(0,10))
-    t=get_ts()
-    # print("random = ",s)
-    # print("ts= ",t)
-    # print("salt= ",t+s)
-    return  t+s
-    # return '15853783488690'
+    def get_sign(self):
+        i=self.salt
+        e=self.get_content
+        s="fanyideskweb" + e + i + "Nw(nmmbP%A-r6U3EUn]Aj"
+        # print("s=", s,"  md5=",get_md5(s))
+        return self.get_md5(s)
+        # return 'b1537e6e7d4296b0145432358da1fce0'
 
 
+    def get_ts(self):
+        t = time.time()
+        ts = str(int(round(t*1000)))
+        print("ts=",ts)
+        return ts
+         #'1584684880395'
 
-def get_sign():
-    return '6b9a6ce3ff157252d9a388c507e70474'
+    # def get_content(self):
+    #     return content
+
+    def yield_form_data(self):
+        form_data={
+            'i': self.content,
+            'from': 'AUTO',
+            'to': 'AUTO',
+            'smartresult': 'dict',
+            'client': 'fanyideskweb',
+            'salt': self.salt,
+            'sign': self.sign,
+            'ts': self.ts,
+            'bv': '9ef61dc3d2f65f61d71a16bd47c6e9ee',#9ef61dc3d2f65f61d71a16bd47c6e9ee,70244e0061db49a9ee62d341c5fed82a
+            'doctype': 'json',
+            'version': '2.1',
+            'keyfrom': 'fanyi.web',
+            'action': 'FY_BY_REALTlME',
+        }
+        return form_data
 
 
-def get_ts():
-    return '1585378348869'
+    def get_headers(self):
+        headers={
+            ' Cookie': ' OUTFOX_SEARCH_USER_ID = -1069162350 @ 10.169.0.102;OUTFOX_SEARCH_USER_ID_NCOO = 1905711305.602343;JSESSIONID = aaaxZffjbdInhgvIqBYfx;___rl__test__cookies = 1586762703312;',
+            'Referer':' http: // fanyi.youdao.com /',
+            'User - Agent':' Mozilla / 5.0(Windows NT 6.1;Win64; x64) AppleWebKit / 537.36(KHTML, likeGecko) Chrome / 72.0.3626.121 Safari / 537.36',
+        }
+        return headers
 
+    def fanyi(self):
+        response = requests.post(self.url, data=self.yield_form_data(), headers=self.get_headers())
+        return response.text
 
-form_data={
-    'i':'我们都爱你',
-    'from': 'AUTO',
-    'to': 'AUTO',
-    'smartresult': 'dict',
-    'client': 'fanyideskweb',
-    'salt': get_salt(),
-    'sign': get_sign(),
-    'ts': get_ts(),
-    'bv': '70244e0061db49a9ee62d341c5fed82a',
-    'doctype': 'json',
-    'version': '2.1',
-    'keyfrom': 'fanyi.web',
-    'action': 'FY_BY_REALTlME',
-}
-response=requests.post(url,data=form_data)
-print(response.text)
+if __name__ == '__main__':
+        # print(form_data)
+        # print(get_headers())
+        # response= requests.post(url, data=form_data, headers=get_headers())
+        # print(response.text)
+        youdao=Youdao('我们')
+        print(youdao.fanyi())
--- test_post_youdao.py
+++ test_post_youdao.py
@@ -5,23 +5,25 @@ from post_youdao import *
 
 class PostYoudaoTest(unittest.TestCase):
     def test_something(self):
-        self.addertEqual(True,True)
+        self.assertEqual(True,True)
 
     def test_get_ts(self):
         # import time
         # ts=time.time()
         # ts=str(int(round(ts*1000)))
         # print(ts)
-        get_ts=mock.Mock(return_value='1585378348869')
-        self.assertEqual('1585378348869',get_ts())
+        get_ts=mock.Mock(return_value='1584684880395')
+        self.assertEqual('1584684880395',get_ts())
 
     def test_get_salt(self):
-        get_salt=mock.Mock(return_value='15853783488690')
-        self.assertEqual('15853783488690',get_salt())
+        get_salt=mock.Mock(return_value='15846848803956')
+        self.assertEqual('15846848803956',get_salt())
 
     def test_get_sign(self):
-        self.assertEqual('6b9a6ce3ff157252d9a388c507e70474',get_sign())
+        get_sign=mock.Mock(return_value="b1537e6e7d4296b0145432358da1fce0")
+        self.assertEqual('b1537e6e7d4296b0145432358da1fce0',get_sign())
+                          # 9e6b89142b28dde4fe3b5c4fc5407b32
 
 
-if __name__ == "__main__":
-    unittest.main()
+if __name__ == '__main__':
+        unittest.main()
